b65477a011bea837204399e6fa03750e4e4de3af
Fabien Franzen <info@atelierfabien.be>
2011-07-28 12:30:16 +0200

    Fixed hooks deprecation using Deface

--- lib/spree_google_merchant_hooks.rb
+++ lib/spree_google_merchant_hooks.rb
@@ -1,5 +1,3 @@
-class SpreeGoogleMerchantHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_configurations_menu do
-    %(<%= configurations_menu_item(t("google_merchant"), admin_google_merchants_path, t("google_merchants_description")) %>)
-  end
-end
+Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :text => %[<%= configurations_menu_item(t("google_merchant"), admin_google_merchants_path, t("google_merchants_description")) %>"])
