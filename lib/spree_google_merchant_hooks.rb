f2d3c0a49b466165ec15a24f06619d0fd518f429
Fabien Franzen <info@atelierfabien.be>
2011-08-08 20:44:27 +0200

    Fixed hook - feeds working properly now too

--- lib/spree_google_merchant_hooks.rb
+++ lib/spree_google_merchant_hooks.rb
@@ -1,3 +1,4 @@
 Deface::Override.new(:virtual_path => "admin/configurations/index",
-                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :text => %[<%= configurations_menu_item(t("google_merchant"), admin_google_merchants_path, t("google_merchants_description")) %>"])
+                     :name => 'google_merchant_configuration',
+                     :insert_bottom => "[data-hook='admin_configurations_menu']",
+                     :text => %[<%= configurations_menu_item(t("google_merchant"), admin_google_merchants_path, t("google_merchants_description")) %>])
